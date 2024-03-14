e9751ba4185defb0742cc3950d53cf0ff150f8a3
Scott Hansen <tech@firecat53.net>
2024-03-14 08:35:51 -0700

    use '127.0.0.1' instead of 'localhost'

--- bwm/__main__.py
+++ bwm/__main__.py
@@ -34,7 +34,7 @@ def port_in_use(port):
 
     """
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        return s.connect_ex(('localhost', port)) == 0
+        return s.connect_ex(('127.0.0.1', port)) == 0
 
 
 def random_str():

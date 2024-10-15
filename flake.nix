71efc0740250019836388eb2641c7fe33ee6a32a
Scott Hansen <tech@firecat53.net>
2024-10-14 20:14:41 -0700

    Update Nix flake setup and workflow

--- flake.nix
+++ flake.nix
@@ -12,34 +12,34 @@
     systems = ["x86_64-linux" "i686-linux" "aarch64-linux"];
     forAllSystems = f:
       nixpkgs.lib.genAttrs systems (system:
-        f rec {
+        f {
           pkgs = nixpkgs.legacyPackages.${system};
-          commonPackages = builtins.attrValues {
-            inherit
-              (pkgs.python3Packages)
-              python
-              pynput
-              xdg-base-dirs
-              ;
-          };
         });
   in {
-    devShells = forAllSystems ({
-      pkgs,
-      commonPackages,
-    }: {
+    devShells = forAllSystems ({pkgs}: {
       default = pkgs.mkShell {
-        packages = commonPackages ++ [pkgs.pandoc];
+        buildInputs = with pkgs; [
+          pandoc
+          python3Packages.venvShellHook
+          uv
+        ];
         shellHook = ''
+          venvShellHook
           alias bwm="python -m bwm"
-          export PYTHONPATH="$PYTHONPATH:$PWD"
         '';
+        venvDir = "./.venv";
+        postVenvCreation = ''
+          uv pip install hatch
+          uv pip install -e .
+          # Prevent venv uv from overriding nixpkgs uv
+          [ -f $(pwd)/.venv/bin/uv ] && rm $(pwd)/.venv/bin/uv*
+        '';
+        C_INCLUDE_PATH = "${pkgs.linuxHeaders}/include";
+        HATCH_ENV_TYPE_VIRTUAL_UV_PATH = "${pkgs.uv}/bin/uv"; # use Nix uv instead of hatch downloaded binary
+        PYTHONPATH = "$PYTHONPATH:$PWD";
       };
     });
-    packages = forAllSystems ({
-      pkgs,
-      commonPackages,
-    }: {
+    packages = forAllSystems ({pkgs}: {
       default = pkgs.python3Packages.buildPythonApplication {
         name = "bitwarden-menu";
         pname = "bitwarden-menu";
@@ -56,7 +56,14 @@
             hatch-vcs
             ;
         };
-        propagatedBuildInputs = commonPackages;
+        propagatedBuildInputs = builtins.attrValues {
+          inherit
+            (pkgs.python3Packages)
+            python
+            pynput
+            xdg-base-dirs
+            ;
+        };
         meta = {
           description = "Dmenu/Rofi/Wofi frontend for managing Bitwarden vaults.";
           homepage = "https://github.com/firecat53/bitwarden-menu";
